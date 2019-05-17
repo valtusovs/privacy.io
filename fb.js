fc405d81388b98df532d215bd6f93c7c0b0daa0f
NoahSfi <36855092+NoahSfi@users.noreply.github.com>
2019-05-17 22:55:10 +0200

    Add files via upload

--- fb.js
+++ fb.js
@@ -1,14 +1,45 @@
-var person = { userID: "",name:"", accessToken:"",picture:""};
+// initialize and setup facebook js sdk
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '2328103577469050',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+    
+};
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
-function logIn(){
+// login with facebook with extra permissions
+function login() {
     FB.login(function(response) {
-        if (response.status == 'connected') {
-        	person.userID = response.authResponse.userID;
-        	person.accessToken = response.authResponse.accessToken;
-        	FB.api('/me?fields = id,name,first_name,email,picture.type(large)',function(userData){
-        		console.log(userData);
-        	})
+        if (response.status === 'connected') {
+            document.getElementById('status').innerHTML = 'We are connected.';
+            document.getElementById('login').style.visibility = 'hidden';
+        } else if (response.status === 'not_authorized') {
+            document.getElementById('status').innerHTML = 'We are not logged in.'
+        } else {
+            document.getElementById('status').innerHTML = 'You are not logged into Facebook.';
         }
-    },{scope: 'public_profile,email'})
-
+    }, {scope: 'email,user_likes,user_birthday,user_hometown'});
 }
+var artist = {name :'',id:''}
+// getting basic user info
+function getInfo() {
+    FB.api('/me', 'GET', {fields: `first_name,last_name,name,id,picture.width(150).height(150),email,location,hometown,music`}, function(response) {
+        console.log(response)
+        document.getElementById('status').innerHTML = "<img src='" + response.picture.data.url + "'>";
+        artist.name = response.music.data[Math.floor(Math.random() * response.music.data.length)].name;
+        artist.id = response.music.data[0].id;
+        
+        link = document.createElement('a')
+        link.href = `https://www.youtube.com/results?search_query=${artist.name}`
+        link.target = '_blank'
+        link.text = 'Listen to music that you may like'
+        youtube = document.getElementById('youtube')
+        youtube.appendChild(link)
