14bc82aaf307bd88d8d7d92596942fea1f320418
Noah Sarfati <noah@noahs-macbook-pro.home>
2019-05-18 01:09:52 +0200

    maj

--- fb.js
+++ fb.js
@@ -31,7 +31,7 @@ function login() {
 
 // getting basic user info
 var artist = {all_name:[],name :''}
-var user = {first_name:'',last_name = '',email = '',location = '',event = {}}
+var user = {first_name:'',last_name : '',email : '',location : '',event : {}}
 function getInfo() {
     FB.api('/me', 'GET', {fields: `first_name,last_name,name,id,picture.width(150).height(150),email,location,hometown,music,events,work,education,sports,movies`}, 
     function(response) {
