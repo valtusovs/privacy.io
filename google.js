9e92745b404c93fd5bb5fd67c15cacb62489722c
Noah Sarfati <noah@noahs-macbook-pro.home>
2019-05-18 13:19:44 +0200

    comment

--- google.js
+++ google.js
@@ -1,7 +1,3 @@
-// Note: This example requires that you consent to location sharing when
-      // prompted by your browser. If you see the error "The Geolocation service
-      // failed.", it means you probably did not give permission for the browser to
-      // locate you.
       var map, infoWindow,panorama;
       function initMap() {
         const base =  {lat: -34.397, lng: 150.644}
