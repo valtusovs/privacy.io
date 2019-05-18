2c26e4887ef6c58505c90c14cdf19da559df8822
Noah Sarfati <noah@noahs-macbook-pro.home>
2019-05-18 11:16:17 +0200

    panorama

--- google.js
+++ google.js
@@ -2,14 +2,16 @@
       // prompted by your browser. If you see the error "The Geolocation service
       // failed.", it means you probably did not give permission for the browser to
       // locate you.
-      var map, infoWindow;
+      const map, infoWindow,panorama;
       function initMap() {
+        const base =  {lat: -34.397, lng: 150.644}
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
+            center: base,
           zoom: 6
         });
         infoWindow = new google.maps.InfoWindow;
 
+        
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
@@ -22,9 +24,19 @@
             infoWindow.setContent('Location found.');
             infoWindow.open(map);
             map.setCenter(pos);
+            panorama = new google.maps.StreetViewPanorama(
+                document.getElementById('pano'), {
+                  position: pos,
+                  pov: {
+                    heading: 34,
+                    pitch: 10
+                  }
+                });
+            map.setStreetView(panorama);
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
           });
+          
         } else {
           // Browser doesn't support Geolocation
           handleLocationError(false, infoWindow, map.getCenter());
